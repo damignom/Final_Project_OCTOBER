d5a486045c7cd5239d1f9f9828a58910ae7a775e
Roma <zhogol.roma1999@gmail.com>
2024-10-19 16:22:41 +0300

    Initial commit

---
+++ src/main/java/com/example/Final_Project_OCTOBER/FinalProjectOctoberApplication.java
@@ -0,0 +1,13 @@
+package com.example.Final_Project_OCTOBER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectOctoberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectOctoberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Final_Project_OCTOBER/FinalProjectOctoberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Final_Project_OCTOBER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectOctoberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
